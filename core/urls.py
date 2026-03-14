f46cf8b0bfc080e17d56afcac0410d37b35ecd39
Walter Licitra <waltercoder84735@gmail.com>
2026-03-14 20:39:49 -0300

    Vistas, URLs y formularios funcionando

---
+++ core/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.inicio, name='inicio'),
+    path('servicios/', views.servicios, name='servicios'),
+    path('blog/', views.blog, name='blog'),
+    path('tutoriales/', views.tutoriales, name='tutoriales'),
+    path('plantillas/', views.plantillas, name='plantillas'),
+    path('contacto/', views.contacto, name='contacto'),
+    path('buscar-cliente/', views.buscar_cliente, name='buscar_cliente'),
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
+from .models import Servicio, Articulo, Cliente, Tutorial, Plantilla
+from .forms import ServicioForm, ArticuloForm, ClienteForm, TutorialForm, PlantillaForm, BusquedaClienteForm
 
-# Create your views here.
+def inicio(request):
+    return render(request, 'inicio.html')
+
+def servicios(request):
+    lista = Servicio.objects.all()
+    form = ServicioForm()
+    if request.method == 'POST':
+        form = ServicioForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request, 'servicios.html', {'servicios': lista, 'form': form})
+
+def blog(request):
+    articulos = Articulo.objects.all()
+    form = ArticuloForm()
+    if request.method == 'POST':
+        form = ArticuloForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request, 'blog.html', {'articulos': articulos, 'form': form})
+
+def tutoriales(request):
+    lista = Tutorial.objects.all()
+    form = TutorialForm()
+    if request.method == 'POST':
+        form = TutorialForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request, 'tutoriales.html', {'tutoriales': lista, 'form': form})
+
+def plantillas(request):
+    lista = Plantilla.objects.all()
+    form = PlantillaForm()
+    if request.method == 'POST':
+        form = PlantillaForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request, 'plantillas.html', {'plantillas': lista, 'form': form})
+
+def contacto(request):
+    form = ClienteForm()
+    if request.method == 'POST':
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'contacto.html', {'form': ClienteForm(), 'exito': True})
+    return render(request, 'contacto.html', {'form': form})
+
+def buscar_cliente(request):
+    resultados = []
+    form = BusquedaClienteForm(request.GET or None)
+    if form.is_valid():
+        nombre = form.cleaned_data['nombre']
+        resultados = Cliente.objects.filter(nombre__icontains=nombre)
+    return render(request, 'buscar_cliente.html', {'form': form, 'resultados': resultados})
